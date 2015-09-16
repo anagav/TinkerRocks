38a1321336bd481cd7fe82147e99eab83eec8857
ashishn <nagav.ashish@gmail.com>
2015-09-16 12:12:11 -0700

    refactorings

--- src/main/java/com/tinkerrocks/storage/StorageConstants.java
+++ src/main/java/com/tinkerrocks/storage/StorageConstants.java
@@ -6,8 +6,6 @@ package com.tinkerrocks.storage;
 public class StorageConstants {
     public static byte PROPERTY_SEPARATOR = '\006';
     public static final String TEST_DATABASE_PREFIX = "/tmp/databases";
-    static final byte[] V_PROPERTY_LIST_TYPE = "LIST".getBytes();
-    static final byte[] V_PROPERTY_SINGLE_TYPE = "SINGLE".getBytes();
     public static final String STORAGE_DIR_PROPERTY = "com.tinkerrocks.storage.dir";
 
 }
